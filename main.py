1a1b7ce93d6c1b8e5350e9ce613dac59d8f3be48
jkyang <yangjingkang@126.com>
2019-10-04 10:45:08 +0800

    1.5 add one condition

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pandas as pd
 import requests
 from openpyxl import load_workbook
 
-from config import codes_path, send_tos, template_path
+from config import codes_path, send_tos, template_path, send_to_myself
 from mail import send_mail
 
 url = 'http://stock.gtimg.cn/data/index.php'
@@ -89,7 +89,7 @@ def download(date, all_codes, over, below):
     return codes
 
 
-def run(date, over=800, below=10000):
+def run(date, over=900, below=10000):
     # 创建存储文件夹
     if not os.path.exists("data"):
         os.makedirs("data")
@@ -103,6 +103,7 @@ def run(date, over=800, below=10000):
     column4 = []
     column5 = []
     column6 = []
+    column7 = []
 
     # 读取待查询股票
     all_data = pd.read_excel(codes_path)
@@ -119,17 +120,17 @@ def run(date, over=800, below=10000):
     codes = [i.name for i in codes_data]
     print(f"已下载 {date} 现手卖盘大于 {over} 小于 {below} 的股票共计 {len(codes)} 个")
 
-    # 情况1-5筛选
+    # 情况1-6筛选
     for code in codes:
         # 读取文件，处理时间
         code_data = pd.read_excel(f"data/{date}/{code}.xls")
         code_data['time'] = pd.to_datetime(code_data['time'], format="%H:%M:%S")
         code_data['time'] = code_data['time'].dt.time
 
-        # 第一种情况 9点35分前有上万白单，全天无大于10001的卖单或者卖单
+        # 第一种情况 9点40分前有上万白单，全天无大于10001的卖单或者卖单
         condition = code_data[
             (code_data["volume"] >= 10000) & (code_data["type"] == "中性盘")
-            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 35))]
+            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
 
         condition2 = code_data[
             (code_data["volume"] > 10001) & (code_data["type"].isin(["买盘", "卖盘"]))
@@ -139,10 +140,10 @@ def run(date, over=800, below=10000):
             column1.append(code)
             continue
 
-        # 第二种情况 9点35分前有连续上千白单，全天无大于9001的买单或者卖单
+        # 第二种情况 9点40分前有连续上千白单，全天无大于9001的买单或者卖单
         condition = code_data[
             (code_data["volume"] >= 1000) & (code_data["type"] == "中性盘")
-            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 35))]
+            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
         condition_index = list(condition.index)
 
         condition2 = code_data[
@@ -155,11 +156,7 @@ def run(date, over=800, below=10000):
                 column2.append(code)
                 continue
 
-        # 第三种情况 9点40分前有上千白单，全天无901以上买单或者卖单
-        condition1 = code_data[
-            (code_data["volume"] >= 1000) & (code_data["type"] == "中性盘")
-            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
-
+        # 全天无901以上买单或者卖单条件限定
         condition2 = code_data[
             (code_data["volume"] >= 901) & (code_data["type"] == "买盘")
             & (code_data["time"] > datetime.time(9, 32)) & (code_data["time"] < datetime.time(14, 30))]
@@ -168,7 +165,15 @@ def run(date, over=800, below=10000):
             (code_data["volume"] >= 901) & (code_data["type"] == "卖盘")
             & (code_data["time"] > datetime.time(9, 32)) & (code_data["time"] < datetime.time(14, 57))]
 
-        if (not condition1.empty) and condition2.empty and condition3.empty:
+        if not (condition2.empty and condition3.empty):
+            continue
+
+        # 第三种情况 9点40分前有上千白单，全天无901以上买单或者卖单
+        condition1 = code_data[
+            (code_data["volume"] >= 1000) & (code_data["type"] == "中性盘")
+            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
+
+        if not condition1.empty:
             column3.append(code)
             continue
 
@@ -179,18 +184,13 @@ def run(date, over=800, below=10000):
 
         condition1_index = list(condition1.index)
 
-        if len(condition1_index) >= 2 and condition2.empty and condition3.empty:
+        if len(condition1_index) >= 2:
             delta = [condition1_index[i + 1] - condition1_index[i] for i in range(len(condition1_index) - 1)]
             if min(delta) <= 6:
                 column4.append(code)
                 continue
 
-        # 第五种情况 9点40分钱有白单（大于10），9点40分前无101以上买单或者卖单，10点前无101以上卖单
-        # 且全天无901以上买单或者卖单
-        condition1 = code_data[
-            (code_data["volume"] > 10) & (code_data["type"] == "中性盘")
-            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
-
+        # 第五种情况 9点40分前无101以上买单或者卖单，10点前无101以上卖单，且全天无901以上买单或者卖单
         condition4 = code_data[
             (code_data["volume"] >= 101) & (code_data["type"].isin(["买盘", "卖盘"]))
             & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
@@ -199,8 +199,27 @@ def run(date, over=800, below=10000):
             (code_data["volume"] >= 101) & (code_data["type"] == "卖盘")
             & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(10, 0))]
 
-        if (not condition1.empty) and condition2.empty and condition3.empty and condition4.empty and condition5.empty:
+        if condition4.empty and condition5.empty:
             column5.append(code)
+            continue
+
+        # 第六种情况 9点40分前有白单（大于等于10），其后有白单（大于等于10），其后有白单（大于等于10），且全天无901以上买单或者卖单
+        condition1 = code_data[
+            (code_data["volume"] >= 10) & (code_data["type"] == "中性盘")
+            & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
+        condition1_index = list(condition1.index)
+        for_query = code_data[
+            (code_data["volume"] >= 10) & (code_data["type"] == "中性盘")
+            & (code_data["time"] > datetime.time(9, 30))]
+        for_query_index = list(for_query.index)
+        added = False
+
+        for i in condition1_index:
+            if len(set(for_query_index) & {i, i + 1, i + 2}) == 3:
+                added = True
+                break
+        if added:
+            column6.append(code)
             continue
 
         # 第六种情况 9点40分前有白单（大于100，小于1000），且全天无901以上买单或者卖单
@@ -208,13 +227,13 @@ def run(date, over=800, below=10000):
             (code_data["volume"] > 100) & (code_data["volume"] < 1000) & (code_data["type"] == "中性盘")
             & (code_data["time"] > datetime.time(9, 30)) & (code_data["time"] < datetime.time(9, 40))]
 
-        if (not condition1.empty) and condition2.empty and condition3.empty:
-            column6.append(code)
+        if not condition1.empty:
+            column7.append(code)
             continue
 
     # 数据汇总
-    final_data = [column1, column2, column3, column4, column5, column6]
-    print(f"情况 1~6 符合条件数目 {[len(item) for item in final_data]}")
+    final_data = [column1, column2, column3, column4, column5, column6, column7]
+    print(f"情况 1~7 符合条件数目 {[len(item) for item in final_data]}")
     final_name_data = [[all_data.loc[i]["name"] for i in j] for j in final_data]
 
     # 写出
@@ -240,7 +259,7 @@ def is_weekday(date):
     return resp.text != '暂无数据'
 
 
-def main(date):
+def main(date, myself):
     date_print = datetime.datetime.strptime(date, "%Y%m%d").strftime("%Y-%m-%d")
     if is_weekday(date):
         if not os.path.isdir(f"data/{date}"):
@@ -253,7 +272,8 @@ def main(date):
         text = f"{date_print}\n" \
                f"总用时 {end - start} s\n" \
                f"请查收附件."
-        send_mail(send_tos=send_tos, name="Simon Yang", subject=f"{date_print}结果", text=text,
+        people_send_to = send_to_myself if myself else send_tos
+        send_mail(send_tos=people_send_to, name="Simon Yang", subject=f"{date_print}结果", text=text,
                   att_urls=[f"result/{date}结果.xlsx", f"result/{date}结果供校验.xlsx"])
     else:
         print(f"{date_print} 休市 无数据")
@@ -264,8 +284,9 @@ if __name__ == '__main__':
     today = datetime.datetime.now().strftime("%Y%m%d")
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--date", help="specify the date")
+    parser.add_argument("-m", "--myself", help="specify which to send to", action="store_true")
     args = parser.parse_args()
     if args.date:
-        main(date=args.date)
+        main(date=args.date, myself=args.myself)
     else:
-        main(date=today)
+        main(date=today, myself=args.myself)
